75fe5378bac929161699c331b73aeebff31ffaac
solomon opeyemi <opeyemisolomon906@gmail.com>
2022-11-15 16:16:56 +0100

    fix linters errors

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-//Create the variables
-let displayMenu = false;                                        //keeps track of the menu state
-const humbergerMenu = document.querySelector('#hum-menu');      //select the humberger menu
-const humbergerImage = document.querySelector('#menu');         //select the humberger image
+// Create the variables
+let displayMenu = false; // keeps track of the menu state
+const humbergerMenu = document.querySelector('#hum-menu'); // select the humberger menu
+const humbergerImage = document.querySelector('#menu'); // select the humberger image
 const cancelImage = document.querySelector('#cancel-menu');
-//nav link
+// const showMenu = document.querySelector('nav-link'); // select the menu options
 
-
-//Create event listeners
-humbergerMenu.addEventListener("click", menu_toogle);
-
-
-
-
-//Create the functions
-function menu_toogle() {
+// Create the functions
+function menuToogle() {
   if (!displayMenu) {
-    humbergerImage.classList.add("not-displaying");
-    cancelImage.classList.remove("not-displaying");
-    cancelImage.classList.add("displayCancel");
-    displayMenu = true
-  }
-  else {
-    humbergerImage.classList.remove("not-displaying");
-    cancelImage.classList.add("not-displaying");
+    humbergerImage.classList.add('not-displaying');
+    cancelImage.classList.remove('not-displaying');
+    cancelImage.classList.add('displayCancel');
+    displayMenu = true;
+  } else {
+    humbergerImage.classList.remove('not-displaying');
+    cancelImage.classList.add('not-displaying');
     displayMenu = false;
   }
-
 }
+
+// Create event listeners
+humbergerMenu.addEventListener('click', menuToogle);
