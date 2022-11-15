3f26eee66c0f42091085600c6b0f2d1607babd1a
Leehaney gee <georgeleehaney20@gmail.com>
2022-11-15 20:28:04 +0300

    Style: Add styles to mobile menu

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ let displayMenu = false; // keeps track of the menu state
 const humbergerMenu = document.querySelector('#hum-menu'); // select the humberger menu
 const humbergerImage = document.querySelector('#menu'); // select the humberger image
 const cancelImage = document.querySelector('#cancel-menu');
-// const showMenu = document.querySelector('nav-link'); // select the menu options
+const showMenu = document.querySelector('#nav-link'); // select the menu options
+const hideMail = document.querySelector('#hide-mail');
 
 // Create the functions
 function menuToogle() {
   if (!displayMenu) {
     humbergerImage.classList.add('not-displaying');
     cancelImage.classList.remove('not-displaying');
-    cancelImage.classList.add('displayCancel');
+    showMenu.classList.remove('hideMenu');
+    showMenu.classList.add('displayMenu');
+    hideMail.classList.add('not-displaying');
     displayMenu = true;
   } else {
     humbergerImage.classList.remove('not-displaying');
     cancelImage.classList.add('not-displaying');
+    showMenu.classList.add('hideMenu');
+    showMenu.classList.remove('displayMenu');
+    hideMail.classList.remove('not-displaying');
     displayMenu = false;
   }
 }
