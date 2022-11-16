f10651e1ca2c15a726ba5f8df7a4ed4896d39afe
Leehaney gee <georgeleehaney20@gmail.com>
2022-11-16 15:01:35 +0300

    feat: add first card dynamically

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const cancelImage = document.querySelector('#cancel-menu');
 const showMenu = document.querySelector('#nav-link'); // select the menu options
 const hideMail = document.querySelector('#hide-mail');
 const navLinks = document.querySelectorAll('.anc-decor');
+const portfolioSection = document.querySelector('#section-bac'); //select the porfolio section
+
+//create javascript object to hold all the information
+let cardInformation = {
+  name: "Multi-Post Stories",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  tech: ["css", "html", "bootstrap", "ruby"],
+  img: ["./assets/first-card.png", "./assets/large.png"],
+  liveVersion: "https://leehaney254.github.io/",
+  source: "https://github.com/leehaney254/leehaney254.github.io",
+}
 
 // Create the functions
 function menuToogle() {
@@ -25,8 +36,28 @@ function menuToogle() {
   }
 }
 
+function addCards() {
+  const div = document.createElement('div');  //create the housing div
+  const imgDiv = document.createElement('div'); //create a div to hold the images
+  const mobileImage = document.createElement('img'); //create the mobile image
+  const desktopImage = document.createElement('img'); //create the desktop image
+  const sideDescription = document.createElement('div'); //create side description 
+  const description = document.createElement('div'); //create side description 
+  const heading =
+
+ //here
+
+}
+
+//call the functions
+addCards();
+
 // Create event listeners
+portfolioSection.addEventListener('click', addCards);
 humbergerMenu.addEventListener('click', menuToogle);
 for (let i = 0; i <= navLinks.length; i++) {
   navLinks[i].addEventListener('click', menuToogle);
 }
+
+
+
